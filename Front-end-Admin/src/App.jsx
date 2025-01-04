1df347679691116404eebc6927d23be91008a46f
SURANGKANANG <ploylol3966@gmail.com>
2025-01-05 04:16:06 +0700

    create page user information

--- Front-end-Admin/src/App.jsx
+++ Front-end-Admin/src/App.jsx
@@ -1,17 +1,16 @@
-import './index.css'; // หรือ './App.css' ถ้าคุณใช้ไฟล์นี้
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserInfor from "./pages/UserInformation/UserInfor";
+import "./index.css";
+
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-yellow-200 flex items-center justify-center">
-      <div className="p-8 max-w-md bg-red-500 text-white rounded-lg shadow-lg text-center">
-        <h1 className="text-4xl font-extrabold underline mb-4">Tailwind CSS</h1>
-        <p className="text-lg">
-          Tailwind CSS is <span className="text-green-300 font-bold">working</span>!
-        </p>
-        <button className="mt-6 px-4 py-2 bg-green-500 text-black font-semibold rounded hover:bg-green-700">
-          Click Me
-        </button>
-      </div>
-    </div>
+    <Router>
+      <Routes> 
+        <Route path="/" element={<UserInfor/>} />  
+        <Route path="/UserInfor" element={<UserInfor />} />
+      </Routes>
+    </Router>
   );
 }
---
+++ Front-end-Admin/src/assets/index.jsx
@@ -0,0 +1,36 @@
+import UserInformation from './icon/Sidebar/UserInformation.png';
+import inspection from './icon/Sidebar/inspection.png';
+import Transferringmoney from './icon/Sidebar/Transferringmoney.png';
+import ReviewManagement from './icon/Sidebar/ReviewManagement.png';
+import Logout from './icon/Sidebar/Logout.png';
+import logo from './images/fillterbar/logo.png';
+import Edit from './icon/UserInformation/Edit.png';
+import FilterDown from './icon/UserInformation/FilterDown.png';
+import Filterslines from './icon/UserInformation/Filterslines.png';
+import search from './icon/UserInformation/search.png';
+import Trash from './icon/UserInformation/Trash.png';
+import member from './images/profile/member.png';
+
+
+
+const Images = {
+  UserInformation,
+  inspection,
+  Transferringmoney,
+  ReviewManagement,
+  logo, 
+  Logout,
+  Edit,
+  FilterDown,
+  Filterslines,
+  search,
+  Trash, 
+  member , 
+
+  
+
+
+
+};
+
+export default Images;
